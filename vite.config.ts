6d977824c29ce30846a56c9a8a26a0f1f4da8a12
xomno01 <113430745+xomno01@users.noreply.github.com>
2025-06-16 22:22:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/index.tsx': path.resolve(__dirname, 'src/index.tsx'),
+    },
+  },
 })
